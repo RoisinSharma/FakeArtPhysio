0562958630b6dced4c881b9166ff8927b79d8d39
Róisín Sharma <rs843@sussex.ac.uk>
2026-02-11 15:41:36 +0000

    fiction.js draft 1

--- experiment/demographics.js
+++ experiment/demographics.js
@@ -110,7 +110,7 @@ const demographics_consent = {
 }
 
 // Demographic info ========================================================================
-//for rs
+////Added 'other' and 'prefer not to say' to gender (not in FakeArt)
 const demographics_questions1 = {
     type: jsPsychSurvey,
     survey_json: {
@@ -160,9 +160,9 @@ const demographics_questions1 = {
                         validators: [
                             {
                                 type: "numeric",
-                                minValue: 18,
+                                minValue: 1,
                                 maxValue: 100,
-                                text: "You must be at least 18 years old to participate.",
+                                text: "Please enter a valid number.",
                             },
                         ],
                     },
@@ -275,170 +275,6 @@ const demographics_questions1 = {
     },
     data: {
         screen: "demographic_questions1",
-    },
-}
-
-//Added 'other' and 'prefer not to say' to gender (not in FakeArt)
-//for fiction.js
-var demographics_questions = {
-    type: jsPsychSurvey,
-    survey_json: {
-        title: "About yourself",
-        completeText: "Continue",
-        pageNextText: "Next",
-        pagePrevText: "Previous",
-        goNextPageAutomatic: false,
-        showQuestionNumbers: false,
-        pages: [
-            {
-                elements: [
-                    {
-                        type: "html",
-                        html: `<div style="text-align: center;">Part 1/4</div>`,
-                    },
-                    {
-                        title: "What is your gender?",
-                        name: "Gender",
-                        type: "radiogroup",
-                        choices: ["Male", "Female", "Other", "Prefer not to say"],
-                        showOtherItem: true,
-                        otherText: "Other",
-                        otherPlaceholder: "Please specify",
-                        isRequired: true,
-                        colCount: 0,
-                    },
-                    {
-                        type: "text",
-                        title: "Please enter your age (in years)",
-                        name: "Age",
-                        isRequired: true,
-                        inputType: "number",
-                        min: 0,
-                        max: 100,
-                        placeholder: "e.g., 21",
-                    },
-                ],
-            },
-            {
-                elements: [
-                    {
-                        title: "What is your highest completed education level?",
-                        name: "Education",
-                        type: "radiogroup",
-                        choices: [
-                            {
-                                value: "Doctorate",
-                                text: "University (doctorate)",
-                            },
-                            {
-                                value: "Master",
-                                text: "University (master)", // "<sub><sup>or equivalent</sup></sub>",
-                            },
-                            {
-                                value: "Bachelor",
-                                text: "University (bachelor)", // "<sub><sup>or equivalent</sup></sub>",
-                            },
-                            {
-                                value: "High school",
-                                text: "High school / Secondary school (or 6th form college)",
-                            },
-                            {
-                                value: "Elementary school",
-                                text: "Elementary school",
-                            },
-                        ],
-                        showOtherItem: true,
-                        otherText: "Other",
-                        otherPlaceholder: "Please specify",
-                        isRequired: true,
-                        colCount: 1,
-                    },
-                    {
-                        visibleIf: "{Education} == 'Doctorate' || {Education} == 'Master' || {Education} == 'Bachelor'",
-                        title: "What is your discipline?",
-                        name: "Discipline",
-                        type: "radiogroup",
-                        choices: [
-                            "Arts and Humanities",
-                            "Media, Communication",
-                            "Literature, Languages",
-                            "History, Archaeology",
-                            "Sociology, Anthropology",
-                            "Political Science, Law",
-                            "Business, Economics",
-                            "Psychology, Neuroscience",
-                            "Medicine",
-                            "Biology, Chemistry, Physics",
-                            "Mathematics, Physics",
-                            "Engineering, Computer Science",
-                        ],
-                        showOtherItem: true,
-                        otherText: "Other",
-                        otherPlaceholder: "Please specify",
-                    },
-                    {
-                        visibleIf: "{Education} == 'High school' || {Education} == 'Master' || {Education} == 'Bachelor'",
-                        title: "Are you currently a student?",
-                        name: "Student",
-                        type: "boolean",
-                        swapOrder: true,
-                        isRequired: true,
-                    },
-                ],
-            },
-            {
-                elements: [
-                    {
-                        title: "How would you describe your ethnicity?",
-                        name: "Ethnicity",
-                        type: "radiogroup",
-                        choices: [
-                            "White",
-                            "Black",
-                            "Hispanic/Latino",
-                            "Middle Eastern/North African",
-                            "South Asian",
-                            "East Asian",
-                            "Southeast Asian",
-                            "Mixed",
-                            "Prefer not to say",
-                        ],
-                        showOtherItem: true,
-                        otherText: "Other",
-                        otherPlaceholder: "Please specify",
-                        isRequired: false,
-                        colCount: 1,
-                    },
-                    {
-                        title: "In which country are you currently living?",
-                        name: "Country",
-                        type: "dropdown",
-                        choicesByUrl: {
-                            url: "https://surveyjs.io/api/CountriesExample",
-                        },
-                        placeholder: "e.g., France",
-                        isRequired: false,
-                    },
-                ],
-            },
-            {
-                elements: [
-                    {
-                        title: "How knowledgeable would you say you are about art (e.g., how familiar with artistic styles, artists or movements)?",
-                        name: "Art_Expertise",
-                        type: "rating",
-                        displayMode: "buttons",
-                        isRequired: true,
-                        minRateDescription: "Not at all",
-                        maxRateDescription: "Very much so",
-                        rateValues: [0, 1, 2, 3, 4, 5, 6],
-                    },
-                ],
-            },
-        ],
-    },
-    data: {
-        screen: "demographic_questions",
     },
 }
 
