13f0caeee957b5ee15b6c490f7852c400900ce14
Róisín Sharma <rs843@sussex.ac.uk>
2026-02-19 17:09:30 +0000

    fix marker port

--- experiment/RestingState.js
+++ experiment/RestingState.js
@@ -9,7 +9,7 @@ function syncLSL() {
             let offsets = []
             for (let i = 0; i < 3; i++) {
                 var startPerf = performance.now()
-                let resp = await fetch("http://139.184.128.202:5000/sync", { cache: "no-store" }) // change IPv4 address as appropriate
+                let resp = await fetch("http://139.184.128.202:5001/sync", { cache: "no-store" }) // change IPv4 address as appropriate
                 let text = await resp.text()
                 var lslTime = parseFloat(text)
                 var endPerf = performance.now()
@@ -31,7 +31,7 @@ function sendMarker(value = "1") {
     // If not synced, still send marker (server will timestamp with local_clock())
     if (lslBaseTime === null) {
         console.warn("LSL not synced yet - sending without JS timestamp")
-        fetch("http://139.184.128.202:5000/marker?value=" + encodeURIComponent(value)) // change IPv4 address as appropriate
+        fetch("http://139.184.128.202:5001/marker?value=" + encodeURIComponent(value)) // change IPv4 address as appropriate
             .then(function () {
                 console.log("sent marker (no-ts)", value)
             })
@@ -42,7 +42,7 @@ function sendMarker(value = "1") {
     }
 
     var ts = lslBaseTime + performance.now() / 1000
-    var url = "http://139.184.128.202:5000/marker?value=" + encodeURIComponent(value) + "&ts=" + encodeURIComponent(ts) // change IPv4 address as appropriate
+    var url = "http://139.184.128.202:5001/marker?value=" + encodeURIComponent(value) + "&ts=" + encodeURIComponent(ts) // change IPv4 address as appropriate
     fetch(url)
         .then(function () {
             console.log("sent marker", value, "ts", ts)
---
+++ experiment/lsl/1_muse_stream.bat
@@ -0,0 +1,3 @@
+MuseLSL2 stream --address 00:55:DA:B5:E8:CF
+
+pause
